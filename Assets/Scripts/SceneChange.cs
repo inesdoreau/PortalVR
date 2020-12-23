62df318f455b0a27fbb37cac10e3125ac719035b
Ines <idoro@hotmail.fr>
2020-12-23 17:21:11 +0100

    Setting the XR plugin and create a menu

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public float delay = 440;
+    public string NewLevel = "Level1";
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NewLevel);
+    }
+}
